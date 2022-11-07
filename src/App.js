a3f909c650474d93ba98f7024bb391b5b8adcc32
HerrySaraswat0708 <harrysaraswat3@gmail.com>
2022-11-07 16:31:03 +0530

    seventh commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import car from './download.png'
+let name = 'Herry'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className='navbar'>
+        <ul className='list-items'><a href='#'>Home</a></ul>
+        <ul className='list-items'><a href='#'>About</a></ul>
+        <ul className='list-items'><a href='#'>Contact</a></ul>
+      </nav>
+      <div className='container'>
+        <div className="App">
+          <h1>Movie Recommendation System</h1>
+          <p>
+            <q>Quotation tag</q>
+          </p>
+          <sub>Subscript</sub> 
+          <sup>Superscript</sup> <br></br>
+          <select>
+            <option selected value='Select'>---Select---</option>
+            <option value='grapefruit'>Grapefruit</option>
+            <option value='lime'>Lime</option>
+            <option value='mango'>Mango</option>
+          </select>
+          <img src={car} className='car'></img>
+        </div>
+        <div className='sidebar'>
+         <h1>Sidebar</h1> 
+         <img src={logo} className='App-logo'></img> 
+        </div>
+        <footer className='footer'>
+          <h4><marquee>This is footer</marquee></h4>
+        </footer>
+      </div>
+    </>
   );
 }
 
